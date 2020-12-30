a9c0060b32f4b9d0211cc7f1cbfb11e47869052e
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-12-30 12:57:57 -0500

    add column four

---
+++ js/api.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.querySelector('#consumirAPI').addEventListener('click', function() {
+    obtenerDatos();
+})
+
+function obtenerDatos() {
+    console.log('Hizo click');
+};
