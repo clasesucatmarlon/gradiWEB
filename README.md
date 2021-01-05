e6d5f75831abb4c83f6eda38eb8dad5cafe65588
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-01-04 19:09:48 -0500

    add styles and code for banner, col2 and col3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 git remote add origin https://github.com/clasesucatmarlon/gradiWEB.git
+
+
+Instalar pack de iconos
+npm i bootstrap-icons
--- js/api.js
+++ js/api.js
@@ -1,40 +1,82 @@
 'use strict';
 
-document.querySelector('#consumirAPI').addEventListener('click', function() {
-    let apiKey = '';
-    let idCity= '5095808';
+/* document.querySelector('#consumirAPI') {
+    let apiKey = '3283ca388af19294b1044e1f4cb98cf4';
+    let idCity = '5095808';
     obtenerDatos(apiKey, idCity);
-})
+} */
 
-function obtenerDatos(apiKey, idCity) {
-    console.log('Hizo click');
+function lanzadera() {
+    let apiKey = '3283ca388af19294b1044e1f4cb98cf4';
+    let idCityBogota = '3688685';
+    let idCityAntartica = '6255152';
 
-/*     let apiKey = '3283ca388af19294b1044e1f4cb98cf4';
-    let idCity= '5095808'; */
+    obtenerDatosTempBanner(apiKey, idCityAntartica);
+}
+
+
+function obtenerDatosTempBanner(apiKey, idCity) {
+
     let url = `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${apiKey}&units=metric`;
 
-
-    let urlBogota = 'https://api.openweathermap.org/data/2.5/weather?id=5095808&appid=3283ca388af19294b1044e1f4cb98cf4&units=metric';
     const api = new XMLHttpRequest();
     api.open('GET', url, true);
 
     api.send();
 
-    api.onreadystatechange = function() {
+    api.onreadystatechange = function () {
         if (this.status == 200 && this.readyState == 4) {
-            console.log();
+
             let datos = JSON.parse(this.responseText);
 
-            let tempMax = datos.main.temp_max;
-            let tempMin = datos.main.temp_min;
+            let tempMax = Math.round(datos.main.temp);
+            let mostrarTemp = document.querySelector('#mostrarTempBanner');
+            mostrarTemp.textContent = `${tempMax}`;
+            
+            let iconDia = datos.weather[0].main;
+            /* iconDia = 'Rain'; */
+            if (iconDia === 'Clouds') {
+                document.getElementById("mostrarImgTempBanner").src="/images/sol-nube1.png"
+            }
+            if (iconDia === 'Clear') {
+                document.getElementById("mostrarImgTempBanner").src="/images/sol.png"
+            }
+            if (iconDia === 'Rain') {
+                document.getElementById("mostrarImgTempBanner").src="/images/lluvia.png"
+            }
+            let mostrarEdoTiempo = document.querySelector('#mostrarEdoTiempo');
+            mostrarEdoTiempo.textContent = `${iconDia}`;
+
+        }
+    }
+};
+
+
+
+function obtenerDatosTempForecast(apiKey, idCity) {
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?id=${idCity}&appid=${apiKey}&units=metric`;
+
+
+
+    const api = new XMLHttpRequest();
+    api.open('GET', url, true);
+
+    api.send();
+
+    api.onreadystatechange = function () {
+        if (this.status == 200 && this.readyState == 4) {
+
+            let datos = JSON.parse(this.responseText);
+
+            let tempMax = Math.round(datos.main.temp_max);
+            let tempMin = Math.round(datos.main.temp_min);
+            let iconDia = datos.weather[0].main;
+            console.log(iconDia)
 
             let mostrarTempMax = document.querySelector('#mostrarTempMax');
-            let mostrarTempMin = document.querySelector('#mostrarTempMin');
 
-            mostrarTempMax.textContent = `Temperatura máxima: ${tempMax}`;
-            mostrarTempMin.textContent = `Temperatura mínima: ${tempMin}`;
-            console.log(tempMax);
-            console.log(tempMin);
+            mostrarTempMax.textContent = `${tempMax}° / ${tempMin}°`;
         }
     }
 };
